95e7dda3d639b562e74fa1f7302dddcc1de96b61
Umar1596 <146162762+Umar1596@users.noreply.github.com>
2023-09-27 10:49:30 +0500

    Add files via upload

---
+++ umar.php
@@ -0,0 +1,26 @@
+<?php
+$maths = 84;
+$computer = 91;
+$english = 87;
+$adds=$computer+$maths+$english;
+echo $adds."<br>";
+?>
+
+<?php
+$maths = 84;
+$computer = 91;
+$english = 87;
+$adds= $computer+$maths+$english;
+$avg=($adds)/3;
+echo $avg."<br>";
+?> 
+
+<?php
+$maths = 84;
+$computer = 91;
+$english = 87;
+if($computer>$maths&$english)
+echo"Computer is Better than Others ";
+else
+echo"Computer is Worse than Others"
+?>
