ee8b556eddf6548523b3108d3fe4ddd8b98766fd
Umar1596 <146162762+Umar1596@users.noreply.github.com>
2023-09-28 11:48:12 +0500

    Add files via upload

---
+++ Calc.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" type="text/css" href="calc.css">
+<?php
+$n1 = "7";
+$n2 = "6";
+
+
+
+echo"<h1>Simple calculator by Umar</h1><br>";
+
+echo "<h2>a = $n1</h2>";
+echo "<h2>b = $n2</h2>";
+
+
+$sum = $n1 + $n2;
+
+echo "<h3>SUM of Two Values is:</h3>  <h4>$sum</h4><br>";
+
+$sub = $n1 - $n2;
+
+echo "<h3>SUBTRACTION of Two Values is:</h3> <h4>$sub</h4><br>";
+
+$mult = $n1 * $n2;
+
+echo "<h3>MULTIPLICATION of Two Values is:</h3> <h4>$mult</h4><br>";
+
+$div = $n1 / $n2;
+
+echo "<h3>DIVISION of Two Values is:</h3> <h4>$div</h4><br>";
+?>
